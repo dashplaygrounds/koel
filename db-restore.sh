68857e09ff10b841c762501cc14ec766da925552
Mark Silverio <dashplaygrounds@gmail.com>
2023-09-08 03:23:07 +0000

    feat: updated db-restore

--- db-restore.sh
+++ db-restore.sh
@@ -2,5 +2,6 @@
 
 pg_restore -v --clean --if-exists \
 	-h localhost -p 5432 -U koel -d koel \
-	~/pglocal-backups/koel-pgdb-09082023_111602.dump
+	/home/client.linux/pglocal-backups/koel-pgdb-09082023_111602.dump
+#	~/pglocal-backups/koel-pgdb-09082023_111602.dump
 echo Done!
